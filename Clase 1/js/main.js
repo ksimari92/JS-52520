0502f7731cabd5f45363e8012bf8894e51308425
Karen Simari <karensimari@live.com.ar>
2023-06-28 08:35:48 -0300

    Clase 1

---
+++ Clase 1/js/main.js
@@ -0,0 +1,30 @@
+let direccion = "Av SiempreViva 1234";
+let numeros = 12;
+let booleanos = true ;
+let booleanos2 = false;
+
+
+direccion = "Libertador 500";
+
+const pi = 3.14;
+
+let resultado = numeros * 2;
+
+console.log("El resultado de la multiplicacion es " + resultado);
+
+console.log(numeros + numeros);
+console.warn("Advertencia")
+console.error("Error")
+
+let datoRecibido = prompt("Que edad tenes?");
+
+let convertir = Number(datoRecibido);
+console.log(convertir);
+
+alert("Hola, bienvenidos a la comi de JS flex!!!");
+
+let confirmar = confirm("Confirma que estuvo en la clase de JS flex nro 1?");
+
+console.log(confirmar);
+
+
